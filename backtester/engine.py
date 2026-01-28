1827fd45ee81263469036c4f9db8bc4ae4381a95
Grace Simonich <gracesimonixh@gmail.com>
2026-01-28 15:11:29 -0800

    Created portfolio class

---
+++ backtester/engine.py
@@ -0,0 +1,5 @@
+from backtester.portfolio import Portfolio
+
+class BTE:
+    def __init__(self, portfolio, strategy):
+        pass
---
+++ backtester/portfolio.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+"""
+Variables/notes
+    1) start_cash ~ The amount of money you have to trade (before any trades are done)
+    2) cash ~ The amount of cash you have
+    3) positions ~ How many shares of each stock owned
+        ex. Ticker   Shares
+             AAPL      10
+             SPY        5
+    4) portfolio_value ~ Total value of portfolio (position (how many shares) * current price of stock)
+    5) equity_curve ~ Record of portfolio value/positions over time
+    6) trades ~ A record of every buy or sell action
+
+
+"""
+
+
+tickers = ['AAPL', 'SPY']
+class Portfolio:
+    def __init__(self, start_cash = 10000):
+        self.cash = start_cash
+        self.positions = pd.Series(0, index=tickers)
+        self.portfolio_value = start_cash
+        self.equity_curve = pd.DataFrame(columns=['Date', 'Cash', 'Portfolio_Value'] + tickers)
+        self.trades = []
+
+    def buy(self, ticker, shares, price, date):
+        if self.cash < price:
+            print("Can't make trade, not enough money")
+        else:
+            self.cash -= (price * shares)
+            self.positions[ticker] += shares
+            self.trades.append({'date': date, 'action': "BUY", 'ticker': ticker, 'shares': shares, 'price': price})
+
+    def sell(self, ticker, shares, price, date):
+        shares_held = self.positions[ticker]
+        if shares_held < shares:
+            print("Can't sell, not enough shares")
+        else:
+            self.cash += (shares *  price)
+            self.positions[ticker] -= shares
+            self.trades.append({'date': date, 'action': "SELL", 'ticker': ticker, 'shares': shares, 'price': price})
+
+    def update(self, current_prices):
+        pass
+
+    def record_equity(self):
+        pass
+
+
