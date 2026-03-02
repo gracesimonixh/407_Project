4f665a5086bd52d56b8908c85285f81b223d0a37
Violette Krueger <violette.krueger@gmail.com>
2026-03-01 16:46:07 -0800

    Update strategies.py
    Added mean reversion

--- backtester/strategies.py
+++ backtester/strategies.py
@@ -64,4 +64,91 @@ class TrendFollowing:
 
         
 class MeanReversion:
-    pass
+    def __init__(self, mean_window=20, threshold_pct=0.02, position_size=10):
+        """"
+        Initializes mean reveresion strategy
+
+        Args:
+            mean_window (int): number of period for moving average 
+            threshold_pct (flaot): percentage distance from mean to trigger entry 
+            position_size (int): number of shares per trade
+        """
+
+        self.mean_window = mean_window 
+        self.threshold_pct = threshold_pct 
+        self.position_size = position_size 
+
+        self.ticker_state = {}
+        self.signals = []
+
+    def process_day(self, ticker, close):
+        """
+        Feeds one day's data to the strategy 
+
+        Args:
+            ticker (str): stock ticker symbol
+            close (float): closing price for the day
+        """
+
+        if ticker not in self.ticker_state:
+            self.ticker_state[ticker] = {
+                'prices': deque(maxlen=self.mean_window),
+                'in_position': False,
+                'position_type': None
+            }
+        
+        state = self.ticker_state[ticker]
+        state['prices'].append(close)
+
+        if len(state['prices']) >= self.mean_window:
+            prices_list = list(state['prices'])
+            mean = sum(prices_list) / self.mean_window
+
+            upper_band = mean * (1 + self.threshold_pct)
+            lower_band = mean * (1 - self.threshold_pct)
+
+            if not state['in_position']:
+
+                if close < lower_band:
+                    signal = {
+                        "ticker": ticker,
+                        "action": "BUY",
+                        "quantity": self.position_size
+                    }
+                    self.signals.append(signal)
+                    state['in_position'] = True
+                    state['position_type'] = "LONG"
+
+                elif close > upper_band:
+                    signal = {
+                        "ticker": ticker,
+                        "action": "SELL",
+                        "quantity": self.position_size
+                    }
+                    self.signals.append(signal)
+                    state['in_position'] = True
+                    state['position_type'] = "SHORT"
+
+            else:
+
+                if state['position_type'] == "LONG" and close >= mean:
+                    signal = {
+                        "ticker": ticker,
+                        "action": "SELL",
+                        "quantity": self.position_size
+                    }
+                    self.signals.append(signal)
+                    state['in_position'] = False
+                    state['position_type'] = None
+
+                elif state['position_type'] == "SHORT" and close <= mean:
+                    signal = {
+                        "ticker": ticker,
+                        "action": "BUY",
+                        "quantity": self.position_size
+                    }
+                    self.signals.append(signal)
+                    state['in_position'] = False
+                    state['position_type'] = None
+
+
