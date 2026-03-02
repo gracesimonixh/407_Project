f92f261ba91cd15b6b2dba76b18860f264b70248
Grace Simonich <gracesimonixh@gmail.com>
2026-03-02 14:30:04 -0800

    Updated trade analysis visuals

--- Visuals/Trade_analysis.py
+++ Visuals/Trade_analysis.py
@@ -3,37 +3,37 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-class TradeReturn:
-    """
-    Bar chart - frequency of winning vs losing trades
-    """
-
-    
-
-class WinLoss:
-    """
-    Pie chart - percentage of winning vs losing trades (win rate)
-    """
-    fig = make_subplots(rows=1, cols=2, subplot_titles=("Trade Outcomes: Trend Following", "Trade Outcome: Mean Reversion"), specs=[[{"type": "pie"}, {"type": "pie"}]])
+def tradeAnalysis():
+    fig = make_subplots(rows=2, cols=2, subplot_titles=("Trade Outcome: Trend Following", "Trade Outcome: Mean Reversion"), 
+                        specs=[[{"type": "pie"}, {"type": "pie"}],
+                                [{"type": "bar"}, {"type": "bar"}]], vertical_spacing=0.05)
     
     df1 = pd.read_csv("results/TrendFollowing_full_trades.csv")
     df1['Result'] = df1['pnl'].apply(lambda x: 'Win' if x > 0 else 'Loss')
     df2 = pd.read_csv("results/MeanReversion_full_trades.csv")
     df2['Result'] = df2['pnl'].apply(lambda x: 'Win' if x > 0 else 'Loss')
-    
 
-    labels = df1['Result'].value_counts().index
-    values = df1['Result'].value_counts().values
+    labels1 = df1['Result'].value_counts().index
+    values1 = df1['Result'].value_counts().values
 
     labels2 = df2['Result'].value_counts().index
     values2 = df2['Result'].value_counts().values
 
-    fig.add_trace(go.Pie(labels=labels, values=values, name="Trend Following", marker=dict(colors=["#f67280", "#5d608d"])), row=1, col=1)
-    fig.add_trace(go.Pie(labels=labels2, values=values2, name="Mean Reversion", marker=dict(colors=["#f67280", "#5d608d"])), row=1, col=2)
+    win_return1 = df1[df1['Result']=='Win']['pnl'].sum()
+    loss_return1 = abs(df1[df1['Result']=='Loss']['pnl'].sum())
 
-    fig.update_layout(height=500, showlegend=False)
-    fig.update_traces(textposition='inside', textinfo='percent+label')
+    win_return2 = df2[df2['Result']=='Win']['pnl'].sum()
+    loss_return2 = abs(df2[df2['Result']=='Loss']['pnl'].sum())
+
+
+    fig.add_trace(go.Pie(labels=labels1, values=values1, name="Trend Following", marker=dict(colors=["#f67280", "#5d608d"])), row=1, col=1)
+    fig.add_trace(go.Pie(labels=labels2, values=values2, name="Mean Reversion", marker=dict(colors=["#f67280", "#5d608d"])), row=1, col=2)
+    
+    fig.add_trace(go.Bar(x=['Wins', 'Losses'], y=[win_return1, loss_return1], name="Trend Following", marker=dict(color=["#f67280", "#5d608d"])), row=2, col=1)
+    fig.add_trace(go.Bar(x=['Wins', 'Losses'], y=[win_return2, loss_return2], name="Mean Reversion", marker=dict(color=["#f67280", "#5d608d"])), row=2, col=2)
+
+    fig.update_layout(height=500, showlegend=False, title={'text':"Trade Anlysis", 'x':0.5, 'xanchor': 'center', 'y': 0.95}, margin=dict(t=100, b=40, l=40, r=40))
+    fig.update_traces(textposition='inside', textinfo='percent+label', selector=dict(type='pie'))
     fig.show()
 
-
-  
+tradeAnalysis()
